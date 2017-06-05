98720b716c26126ea183c669f21440c87bfe7d60
Anton Gavrilov <genezis.od@gmail.com>
2017-06-05 14:54:25 +0300

    continue

--- STM8L_TIMER1/main.c
+++ STM8L_TIMER1/main.c
@@ -8,7 +8,9 @@ LED - PC3
 
 // add some comment in main.c
 
-// adding some functionality in this code-- 
+// adding some functionality in this code--
+
+// continue coding...
 
 #include "iostm8l151g4.h"
 
